0edd44bfd2e4fd36b6734b750380896f54bcc301
Rohit0397 <rohit030897@gmail.com>
2024-04-11 12:35:36 +0530

    deleted some lines

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,11 +1,7 @@
 public class FirstProgram {
     public static void main (String[] args) {
+        // Comment
         System.out.println("Hello Guys");
 
-        int num1 = 6;
-        int num2 = 7;
-        int sum = num1 + num2;
-
-        System.out.print(sum);
     }
 }
