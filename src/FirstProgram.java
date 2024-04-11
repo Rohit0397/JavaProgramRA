c9792b89c166bd73aa7a1b4a0ef7634943f6571c
Rohit0397 <rohit030897@gmail.com>
2024-04-11 12:30:15 +0530

    changes in file

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,5 +1,11 @@
 public class FirstProgram {
     public static void main (String[] args) {
-        System.out.println("Hi my name is Rohit Agrahari");
+        System.out.println("Hello Guys");
+
+        int num1 = 6;
+        int num2 = 7;
+        int sum = num1 + num2;
+
+        System.out.print(sum);
     }
 }
