0489517165e7959e4b22d76a2fe026c6f75ee347
Rohit0397 <rohit030897@gmail.com>
2024-04-24 15:28:30 +0530

    write the table of 2

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,7 +1,10 @@
 public class FirstProgram {
     public static void main (String[] args) {
         // Comment
-        System.out.println("Hello Guys");
-
+        int num = 2;
+        System.out.println("Table of 2:");
+        for(int i = 1; i<=10; i++){
+            System.out.println(num + "*" + i + " = " + (num*i));
+        }
     }
 }
